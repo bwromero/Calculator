570cd9063f25a528dabf9848f1cb172c9031b457
Bryan Romero <bryan.romerodev@gmail.com>
2024-10-28 23:27:47 +0100

    Fixing functions for calculations and entries

--- script.js
+++ script.js
@@ -2,66 +2,62 @@
 const display = document.getElementById('display');
 const buttons = document.querySelectorAll('div.button');
 
-const operators = ['+', '-', '/', '*', '=', 'del', 'C', 'CE', '.']
+let previousNumber = '';
+let currentNumber = '';
+let operation = '';
 
-const result = 0;
 
-let numberEntry = [];
-const operatorEntry = [];
-
-buttons.forEach((el) => {
-    el.addEventListener("click", () => newEntry(el))
-})
-
-function newEntry(entry) {
-
-    const text = entry.textContent;
-
-    if (isNumber(text)) {
-        let currentNumber = numberEntry[0];
-        if(currentNumber) {
-            currentNumber = currentNumber.concat(text);
-            numberEntry.splice(0, 0, currentNumber)
-        } else {
-            numberEntry.push(text);
-        }
-
-        display.innerText = numberEntry[0];
-    }
-
-    if (isOperator(text)) {
-        // clear all entries
-        if(text == 'C') {
-            numberEntry = [];
-            display.innerText = '';
-        }
-
-        // Clear single entry
-        if(text == 'CE') {
-            numberEntry[0] = numberEntry.innerText.slice(0, -1);
-            display.innerText = display.innerText.slice(0, -1);
-        }
-
-        operatorEntry.push(text);
-    }
-
-    console.log("current numberEntry: ", numberEntry[0]);
-    console.log("current operatorEntry: ", operatorEntry);
+function appendNumber(number) {
+    currentNumber += number;
+    updateDisplay();
 }
 
-isNumber = (text) => {
-    if (!isNaN(text) && text !== '') {
-        console.log("Number selected:", text);
-        return true;
-    }
-    return false;
+function setOperation(_operation) {
+
+    if (currentNumber === '') return; // if there's not numbers currently, we cannot display and set an operator
+
+    operation = _operation; 
+
+    previousNumber = currentNumber;
+    display.innerText = currentNumber + operation;
+    currentNumber = '';
 }
 
-isOperator = (text) => {
-    if (operators.includes(text)) {
-        console.log("Operator selected:", text);
-        return true;
+function calculate() {
+    if(previousNumber == '') { // if there's no numbers that has already been inserted, we cannot perform any calculation
+        return;
+    } 
+
+    let result; 
+
+    const prev = parseFloat(previousNumber);
+    const current = parseFloat(currentNumber);
+
+    switch(operation) {
+        case '+':
+            result = prev + current;
+            break;
+        case '-':
+            result = prev - current;
+            break;
+        case '*':
+            result = prev * current;
+            break;
+        case '/':
+            results = prev / current;
+            break;
+        default:
+            return;
     }
 
-    return false;
+    currentNumber = result;
+    operation = '';
+    previousNumber = '';
+    updateDisplay();
+
 }
+
+function updateDisplay() {
+    display.innerText = `${previousNumber} ${operation} ${currentNumber}`;
+}
+
